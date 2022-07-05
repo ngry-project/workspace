b912862bc7400d758b08dddf321f594dc5e39dee
avchugaev <achugaev93@gmail.com>
2022-07-05 23:04:58 +0300

    refactor(translate): :recycle: flatten data flow

--- libs/translate/src/lib/language-source.initializer.ts
+++ libs/translate/src/lib/language-source.initializer.ts
@@ -1,10 +1,9 @@
 import { inject } from '@angular/core';
 import { update } from '@ngry/store';
-import { filter, map, switchMap, take } from 'rxjs';
+import { filter, map, withLatestFrom } from 'rxjs';
 import { AppInitializer } from './app-initializer';
 import { Factory } from './factory';
 import { LANGUAGE_CHANGE_HANDLER } from './language-change-handler.token';
-import { LanguageChangeRequest } from './language-change-request';
 import { LANGUAGE_RESOLVER } from './language-resolver.token';
 import { LANGUAGE_SOURCE } from './language-source.token';
 import { LanguageStore } from './language-store';
@@ -22,13 +21,9 @@ export function LanguageSourceInitializer(): Factory<AppInitializer> {
     return () => {
       const request$ = source.changes.pipe(
         map(resolve),
-        switchMap((target) =>
-          store.pipe(
-            take(1),
-            filter((current) => current !== target),
-            map((current): LanguageChangeRequest => ({ target, current })),
-          ),
-        ),
+        withLatestFrom(store),
+        filter(([target, current]) => target !== current),
+        map(([target, current]) => ({ target, current })),
       );
 
       const language$ = handle(request$);
